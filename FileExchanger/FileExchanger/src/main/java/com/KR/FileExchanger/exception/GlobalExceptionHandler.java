f2904ef7c7a98554c0b5c6410a32496e891d6b23
Каракян Руслан <ruslan01.03@mail.ru>
2025-02-22 00:08:47 +0300

    обновлен ConntrollerAdvice

--- FileExchanger/FileExchanger/src/main/java/com/KR/FileExchanger/exception/GlobalExceptionHandler.java
+++ FileExchanger/FileExchanger/src/main/java/com/KR/FileExchanger/exception/GlobalExceptionHandler.java
@@ -7,10 +7,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<String> handleFileNotFound(FileNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<String> handleFileUploadError(FileUploadException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(FileDeleteException.class)
+    public ResponseEntity<String> handleFileDeleteError(FileDeleteException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        System.err.println("Exception caught: " + e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Internal Server Error: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error: " + e.getMessage());
     }
 }
