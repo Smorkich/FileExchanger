373388965f269b4d10d8a086790aac84a483819d
Каракян Руслан <ruslan01.03@mail.ru>
2025-02-22 00:08:33 +0300

    добавлены более узкие эксепшены

---
+++ FileExchanger/FileExchanger/src/main/java/com/KR/FileExchanger/exception/FileUploadException.java
@@ -0,0 +1,7 @@
+package com.KR.FileExchanger.exception;
+
+public class FileUploadException extends RuntimeException {
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
